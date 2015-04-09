19b19b76fce5d4c35545a5043a317592446873f6
Noah Holmes <noah.r.holmes@gmail.com>
2015-04-09 15:21:21 -0400

    added players with struct

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,16 +1,19 @@
 class TicTacToe
 
-attr_accessor :grid
+attr_accessor :grid, :player1, :player2
+Player = Struct.new(:mark)
 
   def initialize
 	@grid = (1..9).to_a
+	@player1 = Player.new("X")
+	@player2 = Player.new("O")
   end
 
   def display_board
 		grid.each_with_index do |square, index|
 			if (index+1) % 3 == 0
 				print " #{square} \n"
-				print "-----------\n" if index != 8
+				print "-----------\n" if index != 8 # no line on the bottom
 			else
 				print " #{square} |"
 			end
@@ -19,5 +22,5 @@ attr_accessor :grid
 
 end
 
-x = TicTacToe.new
-x.display_board
+play = TicTacToe.new
+play.display_board
