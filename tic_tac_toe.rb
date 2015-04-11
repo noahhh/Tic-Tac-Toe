3246340c7792b9ab6949b1fc9f5478bf0f01bf3d
Noah Holmes <noah.r.holmes@gmail.com>
2015-04-10 22:56:57 -0400

    adding in more methods

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -13,14 +13,29 @@ Player = Struct.new(:mark)
 		grid.each_with_index do |square, index|
 			if (index+1) % 3 == 0
 				print " #{square} \n"
-				print "-----------\n" if index != 8 # no line on the bottom
+				print "-----------\n" if index != 8 # no line on the bottom of the board
 			else
 				print " #{square} |"
 			end
 		end
 	end
 
+  def start_game
+  	puts "\n Where would you like to place your mark?"
+  	display_board
+  	player_pick
+  end
+
+  # def player_pick
+  # 	square = gets.chomp.match(/\d/)[0]
+  # end
+
+  # def valid_pick?
+  # 	square = player_pick
+  # end
+
+
 end
 
 play = TicTacToe.new
-play.display_board
+play.start_game
